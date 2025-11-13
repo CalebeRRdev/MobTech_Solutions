31dca54dec20f4a6285a04bd107aafe421ee5d14
Gabrielrc1 <92831462+Gabrielrc1@users.noreply.github.com>
2025-11-12 21:27:49 -0300

    Delete BACKEND directory

--- BACKEND/src/routes/pontoParada.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (requisicao, resposta) => {
-    resposta.send('Lista de paradas de ônibus')
-})
-
-module.exports = router
